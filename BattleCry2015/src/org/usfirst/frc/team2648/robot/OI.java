3092ce7777aaec7997263383ad9a6975a39f8dc9
Team2648 <frcteam2648@gmail.com>
2016-01-23 15:11:16 -0500

    Test of github

--- BattleCry2015/src/org/usfirst/frc/team2648/robot/OI.java
+++ BattleCry2015/src/org/usfirst/frc/team2648/robot/OI.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
+ * test of GitHub
  */
 public class OI {
     //// CREATING BUTTONS
